8fd745db78f9cade0df24ac56fd18f6692714da3
saeed <s.fathollahzadeh@gmail.com>
2020-04-11 18:33:05 +0200

    add graphs to the latex path

---
+++ Java/myTest.sh
@@ -0,0 +1,8 @@
+./makeClean.sh
+
+./twitterSerialization.sh /mnt/tweets_1M_rows.txt /mnt/javadata1/ 1000
+
+#./experimentReadObjects.sh /mnt/javadata/ /mnt/randomlist
+
+./experimentExternalSort.sh /mnt/javadata/ 5
+
