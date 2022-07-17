ac99a51916a5b94d23bbf88bd6d76d181af76119
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-07-17 02:20:26 +0200

    Update

--- Experiments/run3DownloadData.sh
+++ Experiments/run3DownloadData.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd data
+rm -rf *
 wget https://storage.googleapis.com/tweets_1m/tweets.zip
 wget https://storage.googleapis.com/tweets_1m/random_lists.zip
 
@@ -9,12 +10,14 @@ sudo apt-get install zip -y
 unzip tweets.zip
 unzip random_lists.zip
 
+mv random_lists/*.dat .
 # duplicate data
+mv tweets.txt tweets_1m.txt
 
 cd ..
 SCRIPT="$jCMD  -DinDataPath=data/tweets_1m.txt\
                -DoutDataPath=data/tweets.txt\
                -cp ./SerializationJava.jar at.tugraz.experiments.DataDuplicate
                 "          
-    echo $SCRIPT
+echo $SCRIPT
 time $SCRIPT
