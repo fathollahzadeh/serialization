00a64d284c8a6aecebbd8b8d6d3941a56d264dc1
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-08-13 00:51:03 +0200

    Update Network Scripts

--- Experiments/run6NetworkExperiments.sh
+++ Experiments/run6NetworkExperiments.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# This script runs all local experiments on the specified scale-up machine.
-inDataPathJava="data/tweets.jbin"
+# This script runs all network experiments on the cluster scale-up machines.
+inDataPathMemoryJava="data/tweets.jbin"
+inDataPathDiskJava="data/tweets"
 outDataPath="data/tmp/tweets"
 
 # Load data into memory
 # 1.a Java enviroments
 # write header to log file
 ##########################
-echo baseline,language,plan,machineip,time >>results/Experiment4_ExternalSort_times.dat
+echo "baseline,language,plan,machineip,time" >>results/Experiment4_ExternalSort_times.dat
 
-./expnetwork/runExperiment4_ExternalSort.sh $inDataPathJava $outDataPath m2m Experiment4_ExternalSort_times
-./expnetwork/runExperiment4_ExternalSort.sh $inDataPathJava $outDataPath m2d Experiment4_ExternalSort_times
-./expnetwork/runExperiment4_ExternalSort.sh $inDataPathJava $outDataPath d2m Experiment4_ExternalSort_times
-./expnetwork/runExperiment4_ExternalSort.sh $inDataPathJava $outDataPath d2d Experiment4_ExternalSort_times
+./expnetwork/runExperiment4_ExternalSort_Memory.sh $inDataPathMemoryJava $outDataPath m2m Experiment4_ExternalSort_times
+./expnetwork/runExperiment4_ExternalSort_Memory.sh $inDataPathMemoryJava $outDataPath m2d Experiment4_ExternalSort_times
+
+./expnetwork/runExperiment4_ExternalSort_Disk.sh $inDataPathDiskJava $outDataPath d2m Experiment4_ExternalSort_times
+./expnetwork/runExperiment4_ExternalSort_Disk.sh $inDataPathDiskJava $outDataPath d2d Experiment4_ExternalSort_times
 
