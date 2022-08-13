31fbfdd34436dc946ad0cc7b90ccddd576edf997
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-08-13 19:26:06 +0200

    Update script

--- Experiments/run6NetworkExperiments.sh
+++ Experiments/run6NetworkExperiments.sh
@@ -4,12 +4,16 @@
 inDataPathMemoryJava="data/tweets.jbin"
 inDataPathDiskJava="data/tweets"
 outDataPath="data/tmp/tweets"
+nrow=1000000
 
 # Load data into memory
 # 1.a Java enviroments
 # write header to log file
 ##########################
 echo "baseline,language,plan,machineip,time" >>results/Experiment4_ExternalSort_times.dat
+
+# Serialize data into disk and keep it
+./expnetwork/runExperiment4_Write.sh $inDataPathMemoryJava $outDataPath $nrow 
 
 ./expnetwork/runExperiment4_ExternalSort_Memory.sh $inDataPathMemoryJava $outDataPath m2m Experiment4_ExternalSort_times
 ./expnetwork/runExperiment4_ExternalSort_Memory.sh $inDataPathMemoryJava $outDataPath m2d Experiment4_ExternalSort_times
