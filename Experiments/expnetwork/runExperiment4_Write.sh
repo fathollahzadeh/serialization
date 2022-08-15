e50a15868ef0d63df9a0ad18ee9326b45b36975d
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-08-15 15:47:19 +0200

    Update Scripts Related to C++

---
+++ Experiments/expnetwork/runExperiment4_Write.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+inDataPath=$1
+outDataPath=$2
+nrow=$3
+
+declare -a methods=("Default" "Json+Gzip" "Bson" "ProtoBuf" "Kryo" "ByteBuffer" "Json" "FlatBuffers")
+
+for method in "${methods[@]}"; do
+    for rp in {1..1}; do
+        SCRIPT="$jnCMD -DinDataPath=${inDataPath}\
+                       -DoutDataPath=${outDataPath}.${method}Java\
+                       -Dnrow=${nrow}\
+                       -Dmethod=${method}\
+                       -cp ./SerializationJava.jar at.tugraz.experiments.DataWriteSingle
+                "
+        echo $SCRIPT
+        time $SCRIPT
+    done
+done    
+
