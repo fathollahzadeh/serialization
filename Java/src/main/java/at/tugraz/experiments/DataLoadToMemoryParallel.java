417f6eda15abe1b28804cbefee11901cd8587af6
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-08-16 13:19:58 +0200

    Update LoadToMemory Java

--- Java/src/main/java/at/tugraz/experiments/DataLoadToMemoryParallel.java
+++ Java/src/main/java/at/tugraz/experiments/DataLoadToMemoryParallel.java
@@ -6,8 +6,6 @@ import at.tugraz.util.Const;
 import at.tugraz.util.OptimizerUtils;
 import at.tugraz.util.RootData;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,35 +19,16 @@ public class DataLoadToMemoryParallel {
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 
         String inDataPath = System.getProperty("inDataPath");
-        String seqRand = System.getProperty("seqRand");
         int nrow = Integer.parseInt(System.getProperty("nrow"));
 
         int numThreads = OptimizerUtils.getParallelWriteParallelism();
         ExecutorService pool = CommonThreadPool.get(numThreads);
-        ArrayList<Task> tasks = new ArrayList<>();
+        ArrayList<SerializeTask> tasks = new ArrayList<>();
         int blklen = (int) Math.ceil((double) nrow / numThreads);
 
-        if (seqRand.equalsIgnoreCase("sequential")) {
-            for (int i = 0; i < numThreads & i * blklen < nrow; i++) {
-                ObjectReader reader = new ObjectReader(inDataPath, "Kryo");
-                tasks.add(new SerializeTask(reader, i * blklen, Math.min((i + 1) * blklen, nrow)));
-            }
-        } else {
-            String randomDataPath = System.getProperty("randomDataPath");
-            int[] randomIDs = new int[nrow];
-            try (BufferedReader br = new BufferedReader(new FileReader(randomDataPath))) {
-                String line;
-                int index = 0;
-                while ((line = br.readLine()) != null) {
-                    randomIDs[index++] = Integer.parseInt(line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            for (int i = 0; i < numThreads & i * blklen < nrow; i++) {
-                ObjectReader reader = new ObjectReader(inDataPath, "Kryo");
-                tasks.add(new SerializeTaskRandom(reader ,randomIDs,i * blklen, Math.min((i + 1) * blklen, nrow)));
-            }
+        for (int i = 0; i < numThreads & i * blklen < nrow; i++) {
+            ObjectReader reader = new ObjectReader(inDataPath, "Kryo");
+            tasks.add(new SerializeTask(reader, i * blklen, Math.min((i + 1) * blklen, nrow)));
         }
 
         //wait until all tasks have been executed
@@ -62,22 +41,17 @@ public class DataLoadToMemoryParallel {
         }
     }
 
-    private static abstract class Task implements Callable<Integer> {
+
+    static class SerializeTask implements Callable<Integer> {
         protected final ObjectReader reader;
         protected final int beginPos;
         protected final int endPos;
 
-        public Task(ObjectReader reader, int beginPos, int endPos) {
+        public SerializeTask(ObjectReader reader, int beginPos, int endPos) {
+
             this.reader = reader;
             this.beginPos = beginPos;
             this.endPos = endPos;
-        }
-    }
-
-    static class SerializeTask extends Task {
-
-        public SerializeTask(ObjectReader reader, int beginPos, int endPos) {
-            super(reader, beginPos, endPos);
         }
 
         @Override
@@ -87,23 +61,6 @@ public class DataLoadToMemoryParallel {
                 RootData[] rd = reader.readObjects(i, size);
                 i += rd.length;
                 size = Math.min(endPos - i + 1, Const.BATCHSIZE);
-            }
-            return null;
-        }
-    }
-
-    private static class SerializeTaskRandom extends Task {
-        private final int[] randomList;
-
-        public SerializeTaskRandom(ObjectReader reader, int[] randomList, int beginPos, int endPos) {
-            super(reader, beginPos, endPos);
-            this.randomList = randomList;
-        }
-
-        @Override
-        public Integer call() {
-            for (int i = beginPos; i < endPos; i++) {
-                reader.readObjects(randomList[i], 1);
             }
             return null;
         }
--- Java/src/main/java/at/tugraz/experiments/DataLoadToMemorySingle.java
+++ Java/src/main/java/at/tugraz/experiments/DataLoadToMemorySingle.java
@@ -4,43 +4,19 @@ import at.tugraz.runtime.ObjectReader;
 import at.tugraz.util.Const;
 import at.tugraz.util.RootData;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class DataLoadToMemorySingle {
 
     public static void main(String[] args) {
 
         String inDataPath = System.getProperty("inDataPath");
-        String seqRand = System.getProperty("seqRand");
         int nrow = Integer.parseInt(System.getProperty("nrow"));
         ObjectReader reader = new ObjectReader(inDataPath, "Kryo");
         int size = Const.BATCHSIZE;
 
-        if (seqRand.equalsIgnoreCase("sequential")) {
-            for (int i = 0; i < nrow; ) {
-                RootData[] rd = reader.readObjects(i, size);
-                i += rd.length;
-                size = Math.min(nrow - i + 1, Const.BATCHSIZE);
-            }
-        }
-        else{
-            String randomDataPath = System.getProperty("randomDataPath");
-            int[] randomIDs= new int[nrow];
-            try (BufferedReader br = new BufferedReader(new FileReader(randomDataPath))) {
-                String line;
-                int index=0;
-                while ((line = br.readLine()) != null) {
-                    randomIDs[index++] = Integer.parseInt(line);
-                }
-                for (int i: randomIDs ) {
-                    reader.readObjects(i, 1);
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        for (int i = 0; i < nrow; ) {
+            RootData[] rd = reader.readObjects(i, size);
+            i += rd.length;
+            size = Math.min(nrow - i + 1, Const.BATCHSIZE);
         }
     }
 }
