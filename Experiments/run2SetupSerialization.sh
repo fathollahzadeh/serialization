86aa927d66066f0bc7f5192dc72b4408d21b6b01
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-08-24 22:27:12 +0200

    Minor

--- Experiments/run2SetupSerialization.sh
+++ Experiments/run2SetupSerialization.sh
@@ -6,13 +6,13 @@ rm -rf cppbin
 
 # compile Java baselines
 cd ..
-cd Java
+cd src
 #mvn clean compile assembly:single
 #mv target/Twitter-1.0-SNAPSHOT-jar-with-dependencies.jar ../Experiments/SerializationJava.jar
 
 cd ..
 # compile C++ baslelines
-cd Java/src/main/cpp
+cd src/main/cpp
 # rm -rf CMakeFiles
 # rm -rf cmake_install.cmake
 # rm -rf CMakeCache.txt
