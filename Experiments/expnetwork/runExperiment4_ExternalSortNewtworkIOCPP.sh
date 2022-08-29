a05aef81670caff485798d66bc22a7b1974ffd55
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-08-30 00:50:04 +0200

    Add networkIO benchmark script

---
+++ Experiments/expnetwork/runExperiment4_ExternalSortNewtworkIOCPP.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+method=$1
+inDataPath=$2
+outDataPath=$3
+plan=$4
+log_file_name=$5
+
+IP=$(hostname -I)
+
+for rp in {1..1}; do
+    start=$(date +%s%N)
+    SCRIPT="./cppbin/DataReadNetworkNetworkIO ${inDataPath} ${outDataPath} ${method} ${NETWORKCONFIG} ${plan}"
+    echo $SCRIPT
+    time $SCRIPT
+    end=$(date +%s%N)
+    echo ${method}"CPP,CPP,NetworkIO,"${plan}","${IP}","$((($end - $start) / 1000000)) >>results/$log_file_name.dat
+done    
