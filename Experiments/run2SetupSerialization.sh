f1fabebb5bce55228089d9315ef056e7ef546668
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-09-02 23:17:40 +0200

    up

--- Experiments/run2SetupSerialization.sh
+++ Experiments/run2SetupSerialization.sh
@@ -2,7 +2,7 @@
 
 #cleanup
 rm -rf SerializationJava.jar
-rm -rf cppbin
+#rm -rf cppbin
 cd ..
 
 # compile Java baselines
@@ -10,17 +10,17 @@ mvn clean compile assembly:single
 mv target/Twitter-1.0-SNAPSHOT-jar-with-dependencies.jar Experiments/SerializationJava.jar
 
 # compile C++ baslelines
-cd src/main/cpp
-rm -rf CMakeFiles
-rm -rf cmake_install.cmake
-rm -rf CMakeCache.txt
-rm -rf Makefile
-rm -rf cpp.cbp
-rm -rf bin
-cmake .
-make clean
-make -j12
+# cd src/main/cpp
+# rm -rf CMakeFiles
+# rm -rf cmake_install.cmake
+# rm -rf CMakeCache.txt
+# rm -rf Makefile
+# rm -rf cpp.cbp
+# rm -rf bin
+# cmake .
+# make clean
+# make -j12
 
-mv  bin/ ../../../Experiments/cppbin
+# mv  bin/ ../../../Experiments/cppbin
 
 # compile Rust baselines
