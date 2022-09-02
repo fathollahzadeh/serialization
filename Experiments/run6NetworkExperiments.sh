8037f3326d9634f4dcc8908a354354089635fa2b
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-09-03 01:50:19 +0200

    up

--- Experiments/run6NetworkExperiments.sh
+++ Experiments/run6NetworkExperiments.sh
@@ -6,7 +6,7 @@ inDataPathMemoryCPP="data/tweets.cbin"
 inDataPathDisk="data/tmp/tweets"
 outDataPath="data/tmp/tweets"
 
-declare -a java_methods=("Default") #("Default" "Json+Gzip" "Bson" "ProtoBuf" "Kryo" "ByteBuffer" "Json" "FlatBuffers")
+declare -a java_methods=("Default" "Json+Gzip" "Bson" "ProtoBuf" "Kryo" "ByteBuffer" "Json" "FlatBuffers")
 declare -a cpp_methods=("Handcoded" "inPlace" "Boost" "ProtoBuf" "Bson" "FlatBuf") 
 
 nrow=500000
@@ -47,41 +47,41 @@ for method in "${java_methods[@]}"; do
     #./expnetwork/runExperiment4_ExternalSortNetworkIOJava.sh $method ${outDataPath}.${method}Java ${outDataPath}.${method}SortedJava d2m Experiment4_ExternalSort_times
 
     # 8. IO (d2d)
-   # rm -rf ${outDataPath}.${method}SortedJava
-   #./expnetwork/runExperiment4_ExternalSortNetworkIOJava.sh $method ${outDataPath}.${method}Java ${outDataPath}.${method}SortedJava d2d Experiment4_ExternalSort_times
+    rm -rf ${outDataPath}.${method}SortedJava
+   ./expnetwork/runExperiment4_ExternalSortNetworkIOJava.sh $method ${outDataPath}.${method}Java ${outDataPath}.${method}SortedJava d2d Experiment4_ExternalSort_times
 done    
 
 
-# for method in "${cpp_methods[@]}"; do
-#     #clean up
-#     rm -rf data/tmp
-#     mkdir -p data/tmp
+for method in "${cpp_methods[@]}"; do
+    #clean up
+    rm -rf data/tmp
+    mkdir -p data/tmp
 
-#     # serialize data into disk
-#     ./expnetwork/runExperiment4_WriteCPP.sh $method $inDataPathMemoryCPP ${outDataPath}.${method}CPP $nrow 
+    # serialize data into disk
+    ./expnetwork/runExperiment4_WriteCPP.sh $method $inDataPathMemoryCPP ${outDataPath}.${method}CPP $nrow 
 
-#     # 1. Memory-to-Memory (m2m)
-#     #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2m Experiment4_ExternalSort_times
+    # 1. Memory-to-Memory (m2m)
+    #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2m Experiment4_ExternalSort_times
 
-#     # 2. Memory-to-Disk (m2d)
-#     #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2d Experiment4_ExternalSort_times
+    # 2. Memory-to-Disk (m2d)
+    #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2d Experiment4_ExternalSort_times
 
-#     # 3. Disk-to-Memory (d2m)
-#     #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2m Experiment4_ExternalSort_times
+    # 3. Disk-to-Memory (d2m)
+    #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2m Experiment4_ExternalSort_times
 
-#     # 4. Disk-to-Disk (d2d)
-#     #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
+    # 4. Disk-to-Disk (d2d)
+    #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
 
-#     # 5. IO (m2m)
-#     #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2m Experiment4_ExternalSort_times
+    # 5. IO (m2m)
+    #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2m Experiment4_ExternalSort_times
 
-#     # 6. IO (m2d)
-#     #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2d Experiment4_ExternalSort_times
+    # 6. IO (m2d)
+    #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2d Experiment4_ExternalSort_times
 
-#     # 7. IO (d2m)
-#     #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2m Experiment4_ExternalSort_times
+    # 7. IO (d2m)
+    #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2m Experiment4_ExternalSort_times
 
-#     # 8. IO (d2d)
-#     #rm -rf ${outDataPath}.${method}SortedCPP
-#     ./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
-# done   
+    # 8. IO (d2d)
+    rm -rf ${outDataPath}.${method}SortedCPP
+    ./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
+done   
--- Experiments/runAll.sh
+++ Experiments/runAll.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # clean original results
-#rm -rf results
-#mkdir -p results
+rm -rf results
+mkdir -p results
 
 rm -rf loging.log
 export LOG4JPROP='conf/log4j.properties'
@@ -13,7 +13,7 @@ export jnCMD="java -Xms15g -Xmx15g -Xmn3g --illegal-access=deny -Dlog4j.configur
 #./run1SetupDependencies.sh
 #./run2SetupSerialization.sh
 #./run3DownloadData.sh
-./run4PrepareData.sh
+#./run4PrepareData.sh
 #./run5LocalExperiments.sh
 #./run5LocalExperimentsExternalSort.sh
 ./run6NetworkExperiments.sh
