dd7f085c797d77ba8eacd4b27f910e60ff5e4d1a
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-09-12 12:22:44 +0200

    Update Script

--- Experiments/runAll.sh
+++ Experiments/runAll.sh
@@ -9,14 +9,15 @@ export LOG4JPROP='conf/log4j.properties'
 export NETWORKCONFIG='conf/machines.inf'
 export jCMD="java -Xms15g -Xmx15g -Xmn3g --illegal-access=deny -Dlog4j.configuration=file:$LOG4JPROP"
 export jnCMD="java -Xms15g -Xmx15g -Xmn3g --illegal-access=deny -Dlog4j.configuration=file:$LOG4JPROP"
+export cppValgrind="" #"valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt"
 
 #./run1SetupDependencies.sh
 #./run2SetupSerialization.sh
 #./run3DownloadData.sh
 #./run4PrepareData.sh
-#./run5LocalExperiments.sh
+./run5LocalExperiments.sh
 #./run5LocalExperimentsExternalSort.sh
-./run6NetworkExperiments.sh
+#./run6NetworkExperiments.sh
 
 
 
