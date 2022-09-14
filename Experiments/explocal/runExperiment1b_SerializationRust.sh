9564a48902d5c5ce0660afc797fd7c9a6ed323a5
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-09-14 16:35:59 +0200

    update script

---
+++ Experiments/explocal/runExperiment1b_SerializationRust.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+method=$1
+inDataPath=$2
+nrow=$3
+platform=$4
+task_set=$5
+log_file_name=$6
+
+for rp in {1..1}; do
+    start=$(date +%s%N) #  
+    SCRIPT="./rustbin/DataSerialization${platform} ${inDataPath} ${method} ${nrow}"    
+    if [ "$task_set" = true ] ; then
+        SCRIPT="taskset -c 0 $SCRIPT"
+    fi
+    echo $SCRIPT
+    time $SCRIPT
+    end=$(date +%s%N)
+    echo ${method}"Rust,Rust,"${task_set}",CPU,"${platform}",Sequential,"${nrow}","$((($end - $start) / 1000000)) >>results/$log_file_name.dat
+done
+
+
