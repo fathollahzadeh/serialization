0991f5aab1ef9c13a488c47cbf7e3137beb03b44
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-09-21 00:53:59 +0200

    update rust network reader

--- src/main/rust/src/util/Network.rs
+++ src/main/rust/src/util/Network.rs
@@ -1,4 +1,4 @@
-use std::borrow::Borrow;
+use std::borrow::{Borrow, BorrowMut};
 use std::collections::HashMap;
 use crate::util::MachineInfo::MachineInfo;
 use std::error::Error;
@@ -9,68 +9,62 @@ use csv::Reader;
 
 pub struct Network {
     config: String,
-    objectInEachPage: HashMap<String, MachineInfo>
+    machineInfo: HashMap<String, MachineInfo>,
 }
 
 impl Network {
-    pub fn new(config: String) -> Self{
-        Network::readConfigFile(config.clone());
-        Self{
+    pub fn new(config: String) -> Self {
+        let list = Network::readConfigFile(config.clone());
+        Self {
             config: config,
-            objectInEachPage: Default::default()
+            machineInfo: list,
         }
     }
 
-    fn readConfigFile(config: String) -> io::Result<()> { //-> HashMap<String, MachineInfo>
-        let mut result: HashMap<String, MachineInfo> = HashMap::new();
+    fn readConfigFile(config: String) -> HashMap<String, MachineInfo> { //-> HashMap<String, MachineInfo>
+        let mut list: HashMap<String, MachineInfo> = HashMap::new();
         let mut rootLeaf: HashMap<String, String> = HashMap::new();
 
-        let mut br = csv::Reader::from_path(config.clone())?;
-         for line in br.records() {
-             let record = line?;
-             let ip = record.get(0).unwrap().to_string();
-             let port:u16 = record.get(1).unwrap().parse().unwrap();
-             let root = record.get(2).unwrap().to_string();
-             let nrow:u32 = record.get(3).unwrap().parse().unwrap();
+        let mut br = csv::Reader::from_path(config.clone()).unwrap();
+        for line in br.records() {
+            let record = line.unwrap();
+            let ip = record.get(0).unwrap().to_string();
+            let port: u16 = record.get(1).unwrap().parse().unwrap();
+            let root = record.get(2).unwrap().to_string();
+            let nrow: u32 = record.get(3).unwrap().parse().unwrap();
+            rootLeaf.insert(ip.clone(), root.clone());
 
-             rootLeaf.insert(ip.clone(), root);
-             let nsi= MachineInfo::new(ip.clone(), port, nrow);
-             result.insert(ip, nsi);
-         }
+            let nsi = MachineInfo::new(ip.clone(), port, root, nrow);
+            list.insert(ip, nsi);
+        }
 
-        // for ip in rootLeaf.keys() {
-        //     let root = rootLeaf.get(ip.clone().as_str()).unwrap();
-        //     if root.to_lowercase().eq("root") {
-        //         continue;
-        //     }
-        //     let mut tmpRoot = result.get(root.clone().as_str()).unwrap();
-        //     let mut tmpLeaf = result.get(ip.clone().as_str()).unwrap();
-        //
-        //     tmpRoot.addLeaf(tmpLeaf.to_owned());
-        //     tmpLeaf.set_root(tmpRoot);
-        //     tmp.set_root()
-        // }
-        Ok(())
+        for ip in rootLeaf.keys() {
+            let root = rootLeaf.get(ip.clone().as_str()).unwrap();
+            if root.to_lowercase().eq("root") {
+                continue;
+            }
+
+            list.get_mut(root.clone().as_str()).unwrap().addLeaf(ip.clone());
+            list.get_mut(ip.clone().as_str()).unwrap().set_root(root.clone());
+        }
+
+        for ip in rootLeaf.keys() {
+            let sumLeaf = Network::computeNRow(&list, ip);
+            list.get_mut(ip).unwrap().set_nrowLeaf(sumLeaf);
+        }
+        return list;
+    }
+
+    fn computeNRow(list: &HashMap<String, MachineInfo>, node: &str) -> u32 {
+        let tmp = list.get(node).borrow().unwrap();
+        if tmp.leaves().len() == 0 {
+            return tmp.nrow();
+        } else {
+            let mut sum = tmp.nrow();
+            for leaf in tmp.leaves() {
+                sum += Network::computeNRow(list.borrow(), leaf);
+            }
+            return sum;
+        }
     }
 }
-//  public void readConfigFile(String config) throws IOException {
-
-//
-//         for (String ip: rootLeaf.keySet()){
-//             String root = rootLeaf.get(ip);
-//             if (root.equalsIgnoreCase("root"))
-//                 continue;
-//             machineInfos.get(root).addLeaf(machineInfos.get(ip));
-//             machineInfos.get(ip).setRoot(machineInfos.get(root));
-//         }
-//
-//         for (String ip: machineInfos.keySet()){
-//             int sumLeaf = computeNRow(machineInfos.get(ip)) - machineInfos.get(ip).getNrow();
-//             machineInfos.get(ip).setNrowLeaf(sumLeaf);
-//         }
-//     }
-
-//public Network(String config) throws IOException {
-//         this.config = config;
-//         this.readConfigFile(config);
-//     }
