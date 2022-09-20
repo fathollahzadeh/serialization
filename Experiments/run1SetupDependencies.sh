b73f1f6b7a6a500aa85edf2c17e9c41d43d9bd5c
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-09-20 13:19:34 +0200

    update

--- Experiments/run1SetupDependencies.sh
+++ Experiments/run1SetupDependencies.sh
@@ -64,3 +64,4 @@ make
 sudo make install
 
 # setup Rust dependencies
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- Experiments/run5LocalExperimentsExternalSort.sh
+++ Experiments/run5LocalExperimentsExternalSort.sh
@@ -9,78 +9,76 @@ outExternalSort="data/tmp/externalsort"
 
 declare -a java_methods=("Default" "Json+Gzip" "Bson" "ProtoBuf" "Kryo" "ByteBuffer" "Json" "FlatBuffers") 
 declare -a cpp_methods=("Handcoded" "inPlace" "Boost" "ProtoBuf" "Bson" "FlatBuf") 
-declare -a rust_methods=("Bincode") #("Json" "Bincode" "MessagePack" "Bson" "FlexBuf") 
+declare -a rust_methods=("Json" "Bincode" "MessagePack" "Bson" "FlexBuf") 
 
-externalsort_nrow=1000000
-externalsort_hunk_size=100000
+externalsort_nrow=14000000
+externalsort_hunk_size=2000000
 
 
-# Load data into memory
-# 1.a Java enviroments
 # write header to log file
 ##########################
 echo "baseline,language,taskset,execution,platform,seq_rand,nrow,time" >>results/Experiment3a_ExternalSort_Write_times.dat
 echo "baseline,language,taskset,execution,chunk_size,nrow,time" >>results/Experiment3b_ExternalSort_times.dat
 
-for rp in {1..1}; do
-    # for method in "${java_methods[@]}"; do
-    #     #clean up
-    #     rm -rf data/tmp
-    #     mkdir -p data/tmp
-    #     mkdir -p $outExternalSort
+for rp in {1..3}; do
+    for method in "${java_methods[@]}"; do
+        #clean up
+        rm -rf data/tmp
+        mkdir -p data/tmp
+        mkdir -p $outExternalSort
 
-    #     # External Sort
-    #     ###############   
-    #     ./explocal/runExperiment1c_WriteJava.sh $method $inDataPathJava $outDataPath $externalsort_nrow Single false Experiment3a_ExternalSort_Write_times
+        # External Sort
+        ###############   
+        ./explocal/runExperiment1c_WriteJava.sh $method $inDataPathJava $outDataPath $externalsort_nrow Single false Experiment3a_ExternalSort_Write_times
         
-    #     rm -rf $outExternalSort
-    #     mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times 
+        rm -rf $outExternalSort
+        mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times 
         
-    #     rm -rf $outExternalSort
-    #     mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortIOJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times 
+        rm -rf $outExternalSort
+        mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortIOJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times 
 
-    #     ./explocal/runExperiment1c_WriteJava.sh $method $inDataPathJava $outDataPath $externalsort_nrow Single true Experiment3a_ExternalSort_Write_times
+        ./explocal/runExperiment1c_WriteJava.sh $method $inDataPathJava $outDataPath $externalsort_nrow Single true Experiment3a_ExternalSort_Write_times
        
-    #     rm -rf $outExternalSort
-    #     mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
+        rm -rf $outExternalSort
+        mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
         
-    #     rm -rf $outExternalSort
-    #     mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortIOJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
-    # done
+        rm -rf $outExternalSort
+        mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortIOJava.sh $method ${outDataPath}.${method}Java $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
+    done
 
 
-    # for method in "${cpp_methods[@]}"; do
-    #     #clean up
-    #     rm -rf data/tmp
-    #     mkdir -p data/tmp
+    for method in "${cpp_methods[@]}"; do
+        #clean up
+        rm -rf data/tmp
+        mkdir -p data/tmp
 
-    #     # External Sort
-    #     ###############    
-    #     ./explocal/runExperiment1c_WriteCPP.sh $method $inDataPathCPP $outDataPath $externalsort_nrow Single false Experiment3a_ExternalSort_Write_times
+        # External Sort
+        ###############    
+        ./explocal/runExperiment1c_WriteCPP.sh $method $inDataPathCPP $outDataPath $externalsort_nrow Single false Experiment3a_ExternalSort_Write_times
 
-    #      rm -rf $outExternalSort
-    #      mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortIOCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times 
+         rm -rf $outExternalSort
+         mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortIOCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times 
         
-    #      rm -rf $outExternalSort
-    #      mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times      
+         rm -rf $outExternalSort
+         mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow false $externalsort_hunk_size Experiment3b_ExternalSort_times      
         
 
-    #     ./explocal/runExperiment1c_WriteCPP.sh $method $inDataPathCPP $outDataPath $externalsort_nrow Single true Experiment3a_ExternalSort_Write_times
+        ./explocal/runExperiment1c_WriteCPP.sh $method $inDataPathCPP $outDataPath $externalsort_nrow Single true Experiment3a_ExternalSort_Write_times
         
-    #      rm -rf $outExternalSort
-    #      mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
+         rm -rf $outExternalSort
+         mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
         
-    #      rm -rf $outExternalSort
-    #      mkdir -p $outExternalSort
-    #     ./explocal/runExperiment3_ExternalSortIOCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
-    # done
+         rm -rf $outExternalSort
+         mkdir -p $outExternalSort
+        ./explocal/runExperiment3_ExternalSortIOCPP.sh $method ${outDataPath}.${method}CPP $outExternalSort $externalsort_nrow true $externalsort_hunk_size Experiment3b_ExternalSort_times 
+    done
 
      for method in "${rust_methods[@]}"; do
         #clean up
