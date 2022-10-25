e3b88ed085e0b60025f07d9e4c3eb60d6a7fe567
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-10-25 18:48:16 +0200

    Update Flatbuffers schema

--- compileFlatBuffersJava.sh
+++ compileFlatBuffersJava.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-flatc_path="/home/saeed/Desktop/flatbuffers-master/flatc"
-$flatc_path --java --gen-mutable src/main/java/edu/bu/tweet/flatbuffers/*.fbs
-mv edu/bu/tweet/flatbuffers/* src/main/java/edu/bu/tweet/flatbuffers
-rm -rf edu
+flatc_path="/home/saeed/Apps/flatc"
+
+#$flatc_path --java --gen-mutable src/main/java/at/tugraz/tweet/flatbuffers/*.fbs
+
+
+$flatc_path --java --gen-mutable --gen-object-api --filename-suffix FBS  src/main/java/at/tugraz/tweet/flatbuffers/*.fbs
