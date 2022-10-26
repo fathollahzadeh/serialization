e487057d0f94706512d37de3a772025aad4f316a
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-10-26 02:38:24 +0200

    Set pagesize=256k, network local read=1000

--- src/main/java/at/tugraz/util/Const.java
+++ src/main/java/at/tugraz/util/Const.java
@@ -3,10 +3,10 @@ package at.tugraz.util;
 public class Const {
 
 	// 1 MB Page Size
-	public final static int PAGESIZE = 1024*1024;
+	public final static int PAGESIZE = 256*1024;
 	public final static int BATCHSIZE = 512;
 	public final static int NETWORK_PAGESIZE = 20*1024*1024;
-	public final static int NETWORK_LOCAL_READ_LENGTH = 20000;
+	public final static int NETWORK_LOCAL_READ_LENGTH = 1000;
 	public final static int NETWORK_CLIENT_QUEUE_SIZE = 10;
 	public final static int NETWORK_TIMEOUT = 2000*1000;
 
