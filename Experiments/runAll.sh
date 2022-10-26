d4b46896df1da3108c718b1dd9ce765c695f440c
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-10-26 22:09:56 +0200

    up

--- Experiments/runAll.sh
+++ Experiments/runAll.sh
@@ -15,11 +15,11 @@ export cppValgrind="valgrind --leak-check=full --show-leak-kinds=all --track-ori
 
 #./run1SetupDependencies.sh
 #./run2SetupSerialization.sh
-./run3DownloadData.sh
-./run4PrepareData.sh
-./run5LocalExperiments.sh
+#./run3DownloadData.sh
+#./run4PrepareData.sh
+#./run5LocalExperiments.sh
 #./run5LocalExperimentsExternalSort.sh
-#./run6NetworkExperiments.sh
+./run6NetworkExperiments.sh
 
 
 
