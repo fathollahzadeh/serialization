b800905ff9292e412ac10e1e90ece362979179f6
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-10-26 02:40:32 +0200

    Set C++ pagesize=256k, network local read=1000

--- src/main/cpp/util/header/Const.h
+++ src/main/cpp/util/header/Const.h
@@ -2,10 +2,10 @@
 #define CONST_H 1
 #include <thread>
 
-static const int PAGESIZE = 1024*1024;
+static const int PAGESIZE = 256*1024;
 static const int BATCHSIZE = 512;
 static const int NETWORK_PAGESIZE = 20*1024*1024;
-static const int NETWORK_LOCAL_READ_LENGTH = 20000;
+static const int NETWORK_LOCAL_READ_LENGTH = 1000;
 static const int NETWORK_CLIENT_QUEUE_SIZE = 10;
 static const int NETWORK_TIMEOUT = 2000*1000;
 
