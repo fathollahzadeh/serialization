3f94422a25741d7aeed28d8e61ffdc32a79872b6
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-10-30 23:52:36 +0100

    Add CPP Network Results, update scripts

--- Experiments/runAll.sh
+++ Experiments/runAll.sh
@@ -16,10 +16,10 @@ export cppValgrind="valgrind --leak-check=full --show-leak-kinds=all --track-ori
 #./run1SetupDependencies.sh
 #./run2SetupSerialization.sh
 #./run3DownloadData.sh
-./run4PrepareData.sh
-./run5LocalExperiments.sh
+#./run4PrepareData.sh
+#./run5LocalExperiments.sh
 #./run5LocalExperimentsExternalSort.sh
-#./run6NetworkExperiments.sh
+./run6NetworkExperiments.sh
 
 
 #cssh -l saeed 34.171.107.137 35.224.45.21 34.121.118.248 35.232.102.201 35.202.158.180 34.171.204.131 34.136.129.1 
