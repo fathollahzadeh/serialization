e4107e8cc681ecbb72385b533a9eca846a6fa473
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-10-31 21:22:34 +0100

    minor

--- src/main/java/at/tugraz/experiments/frequency.java
+++ src/main/java/at/tugraz/experiments/frequency.java
@@ -34,72 +34,72 @@ public class frequency {
         AtomicInteger small = new AtomicInteger();
 
 
-        try (Stream<String> stream = Files.lines(Paths.get(inDataPath))) {
-            stream.forEach(e -> {
-                Gson gson = new Gson();
-                TweetStatus tweetStatus = gson.fromJson(e, TweetStatus.class);
-                if (tweetStatus.getExtended_tweet() != null) {
-                    extended_tweet.getAndIncrement();
-                    if (tweetStatus.getExtended_tweet().getEntities() != null) {
-                        extended_tweet_entities.getAndIncrement();
-
-                        if (tweetStatus.getExtended_tweet().getEntities().getMedia() !=null)
-                            for (int i=0; i< tweetStatus.getExtended_tweet().getEntities().getMedia().size(); i++) {
-                                media.getAndIncrement();
-                                if( tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes() !=null) {
-                                    sizes.getAndIncrement();
-                                    tweetStatus.getExtended_tweet().getEntities().getMedia().get(i)
-                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getThumb()!=null){
-                                        thumb.getAndIncrement();
-                                    }
-
-                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getLarge()!=null){
-                                        large.getAndIncrement();
-                                    }
-
-                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getMedium()!=null){
-                                        medium.getAndIncrement();
-                                    }
-
-                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getSmall()!=null){
-                                        small.getAndIncrement();
-                                    }
-                                }
-
-                            }
-
-                        if (tweetStatus.getExtended_tweet().getEntities().getUrls() !=null) {
-                            for (int i = 0; i < tweetStatus.getExtended_tweet().getEntities().getUrls().size(); i++) {
-                                urls.getAndIncrement();
-                            }
-                        }
-
-                        if (tweetStatus.getExtended_tweet().getEntities().getUser_mentions() !=null)
-                            for (int i=0; i< tweetStatus.getExtended_tweet().getEntities().getUser_mentions().size(); i++)
-                                user_mention.getAndIncrement();
-
-                        if (tweetStatus.getExtended_tweet().getEntities().getSymbols() !=null)
-                            for (int i=0; i< tweetStatus.getExtended_tweet().getEntities().getSymbols().size(); i++)
-                                symbol.getAndIncrement();
-
-                    }
-                    if (tweetStatus.getExtended_tweet().getExtendedEntities() != null) {
-                        extended_tweet_extended_entities.getAndIncrement();
-                    }
-                }
-
-//                if (tweetStatus.getEntities() != null) {
-//                    entities.getAndIncrement();
-//                    if (tweetStatus.getEntities().getMedia() !=null)
-//                        for (int i=0; i< tweetStatus.getEntities().getMedia().size(); i++)
-//                            media.getAndIncrement();
+//        try (Stream<String> stream = Files.lines(Paths.get(inDataPath))) {
+//            stream.forEach(e -> {
+//                Gson gson = new Gson();
+//                TweetStatus tweetStatus = gson.fromJson(e, TweetStatus.class);
+//                if (tweetStatus.getExtended_tweet() != null) {
+//                    extended_tweet.getAndIncrement();
+//                    if (tweetStatus.getExtended_tweet().getEntities() != null) {
+//                        extended_tweet_entities.getAndIncrement();
+//
+//                        if (tweetStatus.getExtended_tweet().getEntities().getMedia() !=null)
+//                            for (int i=0; i< tweetStatus.getExtended_tweet().getEntities().getMedia().size(); i++) {
+//                                media.getAndIncrement();
+//                                if( tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes() !=null) {
+//                                    sizes.getAndIncrement();
+//                                    tweetStatus.getExtended_tweet().getEntities().getMedia().get(i)
+//                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getThumb()!=null){
+//                                        thumb.getAndIncrement();
+//                                    }
+//
+//                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getLarge()!=null){
+//                                        large.getAndIncrement();
+//                                    }
+//
+//                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getMedium()!=null){
+//                                        medium.getAndIncrement();
+//                                    }
+//
+//                                    if(tweetStatus.getExtended_tweet().getEntities().getMedia().get(i).getSizes().getSmall()!=null){
+//                                        small.getAndIncrement();
+//                                    }
+//                                }
+//
+//                            }
+//
+//                        if (tweetStatus.getExtended_tweet().getEntities().getUrls() !=null) {
+//                            for (int i = 0; i < tweetStatus.getExtended_tweet().getEntities().getUrls().size(); i++) {
+//                                urls.getAndIncrement();
+//                            }
+//                        }
+//
+//                        if (tweetStatus.getExtended_tweet().getEntities().getUser_mentions() !=null)
+//                            for (int i=0; i< tweetStatus.getExtended_tweet().getEntities().getUser_mentions().size(); i++)
+//                                user_mention.getAndIncrement();
+//
+//                        if (tweetStatus.getExtended_tweet().getEntities().getSymbols() !=null)
+//                            for (int i=0; i< tweetStatus.getExtended_tweet().getEntities().getSymbols().size(); i++)
+//                                symbol.getAndIncrement();
+//
+//                    }
+//                    if (tweetStatus.getExtended_tweet().getExtendedEntities() != null) {
+//                        extended_tweet_extended_entities.getAndIncrement();
+//                    }
 //                }
-
-
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//
+////                if (tweetStatus.getEntities() != null) {
+////                    entities.getAndIncrement();
+////                    if (tweetStatus.getEntities().getMedia() !=null)
+////                        for (int i=0; i< tweetStatus.getEntities().getMedia().size(); i++)
+////                            media.getAndIncrement();
+////                }
+//
+//
+//            });
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         System.out.println();
 
