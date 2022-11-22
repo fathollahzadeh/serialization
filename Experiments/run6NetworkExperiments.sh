6e91ce2c70c03d1f9f457b2ec8dab7286ff7afe2
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2022-11-22 08:39:41 +0100

    update script

--- Experiments/run6NetworkExperiments.sh
+++ Experiments/run6NetworkExperiments.sh
@@ -28,15 +28,15 @@ echo "baseline,language,plan,machineip,time" >>results/Experiment4_ExternalSort_
 #     ./expnetwork/runExperiment4_WriteJava.sh $method $inDataPathMemoryJava ${outDataPath}.${method}Java $nrow 
 # done
 
-for method in "${cpp_methods[@]}"; do    
-    ./expnetwork/runExperiment4_WriteCPP.sh $method $inDataPathMemoryCPP ${outDataPath}.${method}CPP $nrow 
-done
+# for method in "${cpp_methods[@]}"; do    
+#     ./expnetwork/runExperiment4_WriteCPP.sh $method $inDataPathMemoryCPP ${outDataPath}.${method}CPP $nrow 
+# done
 
 # for method in "${rust_methods[@]}"; do
 #     ./expnetwork/runExperiment4_WriteRust.sh $method $inDataPathMemoryRust ${outDataPath}.${method}Rust $nrow 
 # done
 
-#for rp in {1..1}; do
+for rp in {1..1}; do
 
     # for method in "${java_methods[@]}"; do      
 
@@ -70,7 +70,7 @@ done
     # done    
 
 
-    # for method in "${cpp_methods[@]}"; do
+    for method in "${cpp_methods[@]}"; do
         
     #     # 1. Memory-to-Memory (m2m)
     #     #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2m Experiment4_ExternalSort_times
@@ -81,8 +81,8 @@ done
     #     # 3. Disk-to-Memory (d2m)
     #     #./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2m Experiment4_ExternalSort_times
 
-    #     # 4. Disk-to-Disk (d2d)
-    #     ./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
+        # 4. Disk-to-Disk (d2d)
+        ./expnetwork/runExperiment4_ExternalSortCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
 
     #     # 5. IO (m2m)
     #     #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP m2m Experiment4_ExternalSort_times
@@ -93,13 +93,13 @@ done
     #     # 7. IO (d2m)
     #     #./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2m Experiment4_ExternalSort_times
 
-    #     # 8. IO (d2d)
-    #     rm -rf ${outDataPath}.${method}SortedCPP
-    #     ./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
+        # 8. IO (d2d)
+        rm -rf ${outDataPath}.${method}SortedCPP
+        ./expnetwork/runExperiment4_ExternalSortNetworkIOCPP.sh $method ${outDataPath}.${method}CPP ${outDataPath}.${method}SortedCPP d2d Experiment4_ExternalSort_times
 
-    #     #clean-up
-    #     rm -rf ${outDataPath}.${method}SortedCPP
-    # done   
+        #clean-up
+        rm -rf ${outDataPath}.${method}SortedCPP
+    done   
 
 
     #for method in "${rust_methods[@]}"; do
@@ -132,4 +132,4 @@ done
         # # clean-up
         # rm -rf ${outDataPath}.${method}SortedRust
    # done   
-#done    
+done    
