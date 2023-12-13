0017069f139fd866b6f4c6715d350f590b28a7a8
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2023-12-13 14:56:04 -0500

    update write plots

---
+++ Experiments/plotting/update_main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp plots/Exp1_write_single.pdf /home/saeed/Documents/Github/serialization-paper2022/Information-Systems/figures/
+
+cp plots/Exp1_write_parallel.pdf /home/saeed/Documents/Github/serialization-paper2022/Information-Systems/figures/
+
+
+cd /home/saeed/Documents/Github/serialization-paper2022/Information-Systems
+make
