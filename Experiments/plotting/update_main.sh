66b923f3bde902735258024cc22869f9cc90b9bb
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2023-12-13 23:53:25 -0500

    update read sequ and rand diagrams

--- Experiments/plotting/update_main.sh
+++ Experiments/plotting/update_main.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cp plots/Exp1_write_single.pdf /home/saeed/Documents/Github/serialization-paper2022/Information-Systems/figures/
-
 cp plots/Exp1_write_parallel.pdf /home/saeed/Documents/Github/serialization-paper2022/Information-Systems/figures/
+cp plots/Exp2_seq_read_single.pdf /home/saeed/Documents/Github/serialization-paper2022/Information-Systems/figures/
+
+
 
 
 cd /home/saeed/Documents/Github/serialization-paper2022/Information-Systems
